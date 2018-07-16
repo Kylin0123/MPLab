6f0c5d0152a63c789f0ce43ddf203dcb34068b80
Kevin Chen <csczq123456@gmail.com>
2018-07-16 17:32:15 +0800

    优化了标签序号的显示

--- src/main/java/com/nju/mplab14/task6/Task6bReducer.java
+++ src/main/java/com/nju/mplab14/task6/Task6bReducer.java
@@ -8,12 +8,20 @@ import java.io.IOException;
 
 public class Task6bReducer extends Reducer<LongWritable, Text, Text, Text> {
 
+    private long cur = 0;
+    private long cnt = 0;
+
     @Override
     protected void reduce(LongWritable key, Iterable<Text> values, Context context)
         throws IOException, InterruptedException {
 
+        if (cur != key.get()) {
+            cur = key.get();
+            cnt++;
+        }
+
         for(Text value : values){
-            context.write(value, new Text(String.valueOf(key.get())));
+            context.write(value, new Text(String.valueOf(cnt)));
         }
     }
 }

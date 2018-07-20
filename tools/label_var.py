51d32403309d7bcb6f28ed1457b5abc526fa886d
帅 张 <1290645535@qq.com>
2018-07-20 11:38:15 +0800

    新增了分析人物同类数量变化分析alzname；将alz的分析结果进行了降序排序

--- tools/label_var.py
+++ tools/label_var.py
@@ -28,23 +28,43 @@ def get_label_var():
 
 
 def analyze_labels(filename):
-    labels = {}
+    label_dict = {}
     with open(filename, encoding="utf-8") as f:
         for line in f:
             splt = line[:-1].split("\t")
             name = splt[0]
             label = splt[1]
-            if label in labels:
-                labels[label].append(name)
+            if label in label_dict:
+                label_dict[label].append(name)
             else:
-                labels[label] = [name]
-    for label in labels:
-        print(label+":("+str(len(labels[label]))+")\t"+str(labels[label]))
+                label_dict[label] = [name]
+    return label_dict
+
+
+def gen_name_dict(filename):
+    name_dict = {}
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            splt = line[:-1].split("\t")
+            name = splt[0]
+            label = splt[1]
+            name_dict[name] = label
+    return name_dict
+
+
+def analyze_labels_var(name):
+    for i in range(0, len(os.listdir("output5-itr"))):
+        name_dict = gen_name_dict("output5-itr/itr"+str(i)+"/part-r-00000")
+        label_dict = analyze_labels("output5-itr/itr"+str(i)+"/part-r-00000")
+        print("itr"+str(i)+":"+str(len(label_dict[name_dict[name]])))
+    name_dict = gen_name_dict("output5/part-r-00000")
+    label_dict = analyze_labels("output5/part-r-00000")
+    print("final:"+str(len(label_dict[name_dict[name]])))
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("请输入参数：num/var/alz")
+        print("请输入参数：num/var/alz/alzname")
     elif sys.argv[1] == 'var':
         get_label_var()
     elif sys.argv[1] == 'num':
@@ -56,4 +76,15 @@ if __name__ == '__main__':
         if len(sys.argv) < 3:
             print("请输入文件名")
         else:
-            analyze_labels(sys.argv[2])
+            labels = analyze_labels(sys.argv[2])
+            label_list = []
+            for label in labels:
+                label_list.append((label, labels[label]))
+            label_list.sort(key=lambda x: len(x[1]), reverse=True)
+            for label, names in label_list:
+                print("label "+str(label)+" ("+str(len(names))+") "+str(names))
+    elif sys.argv[1] == 'alzname':
+        if len(sys.argv) < 3:
+            print("请输入人物名")
+        else:
+            analyze_labels_var(sys.argv[2])
